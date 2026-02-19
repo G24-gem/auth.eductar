e88b730addabcfd01c907197c71b65d71e840b79
FEMI <techbyfemi@gmail.com>
2026-02-19 01:17:49 +0100

    nothin serious

--- config/DB.js
+++ config/DB.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1); // stops the app if connection fails
-  }
+   const conn = await mongoose.connect(process.env.MONGO_URI);
+   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
+
+module.exports = connectDB;
